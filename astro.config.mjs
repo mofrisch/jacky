97da1d415ffd8b3196cf94ee0917805ca7e44bbd
Moritz Frisch <mo_68@gmx.de>
2024-03-10 15:58:15 +0100

    add vercel analytics and speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [vue(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
