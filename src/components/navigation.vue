35f9243a73d8092c9e2df36675cb40ff67763432
Moritz Frisch <mo_68@gmx.de>
2024-03-10 18:56:21 +0100

    check vue.js

---
+++ src/components/navigation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav>
+      <button role="button" @click="openMenu(true)" v-if="!isMenuOpen">
+        Open Menu
+      </button>
+      <button role="button" @click="openMenu(false)" v-else>
+        Close Menu
+      </button>
+      <ul v-if="isMenuOpen">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isMenuOpen = ref<boolean>(false)
+
+const openMenu = (action: boolean) => {
+  isMenuOpen.value = action;
+}
+</script>
