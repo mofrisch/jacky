8a9588a8093421da5efb803774b58d1a1ffdf819
Moritz Frisch <mo_68@gmx.de>
2024-03-12 17:30:56 +0100

    contact form validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -53,7 +53,7 @@
         <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
           <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div>
-              <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">Vorname</label>
+              <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">Vorname *</label>
               <div class="mt-2.5">
                 <input type="text" name="first-name" id="first-name" autocomplete="given-name" required
                   class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
@@ -67,7 +67,7 @@
               </div>
             </div>
             <div class="sm:col-span-2">
-              <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+              <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email *</label>
               <div class="mt-2.5">
                 <input type="email" name="email" id="email" autocomplete="email" required
                   class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
@@ -81,7 +81,7 @@
               </div>
             </div>
             <div class="sm:col-span-2">
-              <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Deine Nachricht</label>
+              <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Deine Nachricht *</label>
               <div class="mt-2.5">
                 <textarea name="message" id="message" rows="4" required
                   class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
